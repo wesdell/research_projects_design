47c974ad2d3db8d6da64eac17274624f6ed9df11
Wesdell <wesdell17@gmail.com>
2024-08-03 17:56:34 -0500

    Set up new test to accept a project if there is an optional codirector

--- tests/ProjectRequirementsTest.java
+++ tests/ProjectRequirementsTest.java
@@ -19,4 +19,22 @@ public class ProjectRequirementsTest {
 
         assertEquals(1, institution.getProjects().size());
     }
+
+    @Test
+    public void Given_ProjectDirectorAndCodirector_When_ProjectAskForValidation_Then_ItIsAccepted() {
+        Institution institution = new Institution("EPN");
+        TitularTeacher lucas = new TitularTeacher("Lucas");
+        TitularTeacher edwin = new TitularTeacher("Edwin");
+
+        institution.addProject(
+            new InternalProject(
+                "Project 1",
+                lucas,
+                edwin,
+                5000.00
+            )
+        );
+
+        assertEquals(1, institution.getProjects().size());
+    }
 }
