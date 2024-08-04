9886079a14c26b09f1db71222dc40cbc17ebfbba
Wesdell <wesdell17@gmail.com>
2024-08-03 20:09:30 -0500

    Add new test to not accept an Internal project if it exceeds the maximum number of collaborators

--- tests/InternalProjectTest.java
+++ tests/InternalProjectTest.java
@@ -1,2 +1,29 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class InternalProjectTest {
+    @Test
+    public void Given_InternalProjectAndMoreThanTwoInvestigators_When_ProjectAskForValidation_Then_ItIsNotAccepted() {
+        Institution institution = new Institution("EPN");
+        TitularTeacher lucas = new TitularTeacher("Lucas");
+        TitularTeacher edwin = new TitularTeacher("Edwin");
+        OccasionalTeacher david = new OccasionalTeacher("David");
+
+        try {
+            institution.addProject(
+                new InternalProject(
+                    "Project 1",
+                    lucas,
+                    edwin,
+                    5000.00,
+                    david
+                )
+            );
+        } catch (ExceedCollaboratorsException e) {
+            System.out.println(e);
+        }
+
+        assertEquals(0, institution.getProjects().size());
+    }
 }
