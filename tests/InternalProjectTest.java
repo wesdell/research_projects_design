09ed9f7c3ccc111bef1da9de57729fb839454070
Wesdell <wesdell17@gmail.com>
2024-08-09 16:34:24 -0500

    Move tests to InternalProjectTest and refactor

--- tests/InternalProjectTest.java
+++ tests/InternalProjectTest.java
@@ -4,6 +4,40 @@ import static org.junit.Assert.assertEquals;
 
 public class InternalProjectTest {
     @Test
+    public void Given_ProjectAndDirector_When_ProjectAskForValidation_Then_ItIsAccepted() {
+        Institution institution = new Institution("EPN");
+        TitularTeacher lucas = new TitularTeacher("Lucas");
+
+        institution.addProject(
+            new InternalProject(
+                "Project 1",
+                lucas,
+                5000.00
+            )
+        );
+
+        assertEquals(1, institution.getProjects().size());
+    }
+
+    @Test
+    public void Given_ProjectDirectorAndCodirector_When_ProjectAskForValidation_Then_ItIsAccepted() {
+        Institution institution = new Institution("EPN");
+        TitularTeacher lucas = new TitularTeacher("Lucas");
+        TitularTeacher edwin = new TitularTeacher("Edwin");
+
+        institution.addProject(
+            new InternalProject(
+                "Project 1",
+                lucas,
+                edwin,
+                5000.00
+            )
+        );
+
+        assertEquals(1, institution.getProjects().size());
+    }
+
+    @Test
     public void Given_InternalProjectAndMoreThanTwoInvestigators_When_ProjectAskForValidation_Then_ItIsNotAccepted() {
         Institution institution = new Institution("EPN");
         TitularTeacher lucas = new TitularTeacher("Lucas");
